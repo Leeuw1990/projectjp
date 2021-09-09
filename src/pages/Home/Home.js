513def6a9c157377fc0656b47fd000c59366c6f0
Leeuw1990 <74967503+Leeuw1990@users.noreply.github.com>
2021-09-09 17:36:54 +0200

    small changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import jpImage from '../../assets/jpmuziekmaken.jpg';
 import styles from './home.module.css'
+import ContentBlock from "../../components/ContentBlock/ContentBlock";
 
 function Home() {
     return(
         <div className={styles.home}>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid aperiam atque esse et illo iure nobis non placeat. Iure qui sapiente similique sit vero. Consequatur doloremque non reiciendis rerum!</h3>
+            <ContentBlock
+                headerText='Home'
+                text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid distinctio dolor est facilis maxime recusandae repellat saepe veniam voluptatum? Minima natus numquam obcaecati officia possimus praesentium quis reiciendis, tempore? Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid distinctio dolor est facilis '
+            />
             <img className={styles.picture} alt='foto van jp' src={jpImage}/>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid aperiam atque esse et illo iure nobis non placeat. Iure qui sapiente similique sit vero. Consequatur doloremque non reiciendis rerum!</h3>
+            <ContentBlock
+                headerText='Home'
+                text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid distinctio dolor est facilis maxime recusandae repellat saepe veniam voluptatum? Minima natus numquam obcaecati officia possimus praesentium quis reiciendis, tempore? Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid distinctio dolor est facilis '
+            />
         </div>
     );
 }
